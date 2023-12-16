f556d8801352282b9f03075f11575c1e88c2dbd7
Mariia Maksimova <mariia.maksimova@transferwise.com>
2023-12-16 22:10:10 +0000

    initial commit

--- src/main/java/guru/springframework/spring5dragonfly/Spring5dragonflyApplication.java
+++ src/main/java/guru/springframework/spring5dragonfly/Spring5dragonflyApplication.java
@@ -1,12 +1,12 @@
-package guru.springframework.spring5webapp;
+package guru.springframework.spring5dragonfly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5webappApplication {
+public class Spring5dragonflyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
+		SpringApplication.run(Spring5dragonflyApplication.class, args);
 	}
 }
--- src/test/java/guru/springframework/spring5dragonfly/Spring5DragonflyApplicationTests.java
+++ src/test/java/guru/springframework/spring5dragonfly/Spring5DragonflyApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp;
+package guru.springframework.spring5dragonfly;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Spring5webappApplicationTests {
+public class Spring5DragonflyApplicationTests {
 
 	@Test
 	public void contextLoads() {
