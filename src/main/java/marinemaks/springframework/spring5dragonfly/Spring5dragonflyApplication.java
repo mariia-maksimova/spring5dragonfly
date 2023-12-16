b85f0c32698b15fd514acb44f7562127480d805d
Mariia Maksimova <mariia.maksimova@transferwise.com>
2023-12-16 22:25:54 +0000

    make things better
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

---
+++ src/main/java/marinemaks/springframework/spring5dragonfly/Spring5dragonflyApplication.java
@@ -0,0 +1,13 @@
+package marinemaks.springframework.spring5dragonfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5dragonflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5dragonflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marinemaks/springframework/spring5dragonfly/Spring5dragonflyApplicationTests.java
@@ -0,0 +1,13 @@
+package marinemaks.springframework.spring5dragonfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5dragonflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
